e2a0f1ccfdbac31e049706a1a3caa9324ced6c0d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-05-31 03:15:14 +0300

    Solve betty doc error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates binary tree node with left
+ * and right pointers set to null.
+ * @parent: pointer to the parent of the newly created node.
+ * @value: value to add to the n variable of the newly created node.
+ * Return: pointer to the created node.
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
