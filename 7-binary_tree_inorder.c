0d0cf6356a18c0374f7e0e5f88adbb8c272ccfac
Juliusmwash <cutejuliusmwash@gmail.com>
2023-05-31 06:33:09 +0300

    Solve task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes through a binary tree using
+ * binary_tree_inorder - goes through a binary tree using
  * pre-order traversal.
  * @tree: pointer to the root of the binary tree.
  * @func: function which is being passed the node's value.
  * Return: Nothing.
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
 	{
+                binary_tree_inorder(tree->left, func);
 		func(tree->n);
-                binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
         }
 }
