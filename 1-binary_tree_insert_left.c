ce16d938bc5cc1b09ac3c06f4ec36dbf5592d043
Juliusmwash <cutejuliusmwash@gmail.com>
2023-05-31 04:21:43 +0300

    Edit file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 	if (parent->left != NULL)
+	{
 		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
 
 	parent->left = newNode;
 
