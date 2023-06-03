7e7d77dce3fb2e6ae870afc8bd0f24bdbeba4989
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-03 23:28:41 +0300

    Edit file

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -4,20 +4,22 @@
  * bst_insert_helper - inserts the node at the suppossed location.
  * @tree: double pointer to the root node of a binary search tree.
  * @newNode: node to insert in a binary search tree
+ * @check: 0 if value not in bst.
+ * 	Otherwise adjusted to 1 if value exist in bst
  * Return: pointer to the root node of a binary search tree.
  */
 
-bst_t *bst_insert_helper(bst_t **tree, bst_t *newNode)
+bst_t *bst_insert_helper(bst_t **tree, bst_t *newNode, int *check)
 {
 	if (!(*tree))
 	{
 		*tree = newNode;
 		return (*tree);
 	}
-	if (newNode->n < (*tree)->n)
+	else if (newNode->n < (*tree)->n)
 	{
 		if ((*tree)->left)
-			bst_insert_helper(&(*tree)->left, newNode);
+			bst_insert_helper(&(*tree)->left, newNode, check);
 		else
 		{
 			newNode->parent = (*tree);
@@ -27,14 +29,19 @@ bst_t *bst_insert_helper(bst_t **tree, bst_t *newNode)
 	else if (newNode->n > (*tree)->n)
 	{
 		if ((*tree)->right)
-			bst_insert_helper(&(*tree)->right, newNode);
+			bst_insert_helper(&(*tree)->right, newNode, check);
 		else
 		{
 			newNode->parent = (*tree);
 			(*tree)->right = newNode;
 		}
 	}
-	return (*tree);
+	else
+	{
+		*check = 1;
+		return (NULL);
+	}
+	return (newNode);
 }
 
 /**
@@ -47,10 +54,16 @@ bst_t *bst_insert_helper(bst_t **tree, bst_t *newNode)
 bst_t *bst_insert(bst_t **tree, int value)
 {
 	bst_t *newNode;
+	int check = 0;
 
 	newNode = binary_tree_node(NULL, value);
 	if (!newNode)
 		return (NULL);
-	bst_insert_helper(tree, newNode);
+	bst_insert_helper(tree, newNode, &check);
+	if (check)
+	{
+		free(newNode);
+		return (NULL);
+	}
 	return (newNode);
 }
