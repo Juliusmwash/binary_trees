d6a158913300aba098298da8478f84feeb904e23
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-08 02:14:53 +0300

    Edit task 30

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -64,10 +64,44 @@ void satisfy_avl_right_tree(bst_t *tree, int rootValue, int *avlValid)
 	satisfy_avl_right_tree(tree->left, rootValue, avlValid);
 	satisfy_avl_right_tree(tree->right, rootValue, avlValid);
 }
+void avl_duplicate_check(const bst_t *tree, int value, int *dupCheck)
+{
+	if (!tree)
+		return;
+	if ((tree->left && value == tree->left->n) ||
+			(tree->right && value == tree->right->n))
+	{
+		*dupCheck = 1;
+		return;
 
+	}
+	avl_duplicate_check((const bst_t *) tree->left, value, dupCheck);
+	avl_duplicate_check((const bst_t *) tree->right, value, dupCheck);
+}
+int avl_duplicate_check_caller(const bst_t *tree, int *dupCheck)
+{
+	int value;
+
+	if (tree)
+	{
+		value = tree->n;
+		avl_duplicate_check(tree, value, dupCheck);
+		if (!(*dupCheck))
+		{
+			avl_duplicate_check_caller(tree->left, dupCheck);
+			avl_duplicate_check_caller(tree->right, dupCheck);
+		}
+	}
+	return (*dupCheck);
+
+}
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int balanceCheck = 0, avlValid = 0;
+	int balanceCheck = 0, avlValid = 0, dupCheck = 0;
+
+	avl_duplicate_check_caller(tree, &dupCheck);
+	if (dupCheck)
+		return (0);
 
 	satisfy_avl_left_tree(tree->left, tree->n, &avlValid);
 	if (avlValid)
