e61a035d409216978b76758418b8aad27ad2d12d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-08 03:06:46 +0300

    Edit task 30

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,26 +1,42 @@
 #include "binary_trees.h"
 
+/**
+ * node_balance_factor - finds the height which is used to find the node's
+ * balance factor.
+ * @tree: root node of an avl tree.
+ * Return: height.
+ */
 
 size_t node_balance_factor(const binary_tree_t *tree)
 {
-        int leftHeight = 0, rightHeight = 0;
+	int leftHeight = 0, rightHeight = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left)
-                leftHeight = 1 + node_balance_factor(tree->left);
-        else
-                leftHeight = 1;
-        if (tree->right)
-                rightHeight = 1 + node_balance_factor(tree->right);
-        else
-                rightHeight = 1;
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		leftHeight = 1 + node_balance_factor(tree->left);
+	else
+		leftHeight = 1;
 
-        if (leftHeight > rightHeight)
-                return (leftHeight);
+	if (tree->right)
+		rightHeight = 1 + node_balance_factor(tree->right);
+	else
+		rightHeight = 1;
 
-        return (rightHeight);
+	if (leftHeight > rightHeight)
+		return (leftHeight);
+
+	return (rightHeight);
 }
+
+/**
+ * binary_tree_is_avl_check - finds the base factors of tree nodes and based on
+ * the found factors determines if a tree is a valid avl tree or not.
+ * @tree: pointer to the root node of a tree to check its avl eligibility.
+ * @balCheck: this is the final decission on whether a tree is avl or not.
+ * if balCheck is 0, tree is an avl tree. If 1, tree is not an avl tree.
+ * Return: Nothing.
+ */
 
 void binary_tree_is_avl_check(const binary_tree_t *tree, int *balCheck)
 {
@@ -37,8 +53,16 @@ void binary_tree_is_avl_check(const binary_tree_t *tree, int *balCheck)
 		binary_tree_is_avl_check(tree->left, balCheck);
 		binary_tree_is_avl_check(tree->right, balCheck);
 	}
-	return;
 }
+
+/**
+ * satisfy_avl_left_tree - checks whether the values on the left subtree of the
+ * root node breaks the rules of an avl tree (must be less than the root node).
+ * @tree: pointer to the root node of the tree to be satisfied.
+ * @rootValue: value of the root node of the tree.
+ * @avlValid: if 0 tree is an avl tree, else tree is not an avl tree.
+ * Return: nothing.
+ */
 
 void satisfy_avl_left_tree(bst_t *tree, int rootValue, int *avlValid)
 {
@@ -52,6 +76,16 @@ void satisfy_avl_left_tree(bst_t *tree, int rootValue, int *avlValid)
 	satisfy_avl_left_tree(tree->left, rootValue, avlValid);
 	satisfy_avl_left_tree(tree->right, rootValue, avlValid);
 }
+
+/**
+ * satisfy_avl_right_tree - does exactly like the 'satisfy_avl_left_tree'
+ * but on the right subtree of the root node of a tree.
+ * @tree: pointer to the root node of the tree to be satisfied.
+ * @rootValue: value of the root node of the tree.
+ * @avlValid: if 0 tree is an avl tree, else tree is not an avl tree.
+ * Return: nothing.
+ */
+
 void satisfy_avl_right_tree(bst_t *tree, int rootValue, int *avlValid)
 {
 	if (tree == NULL)
@@ -64,6 +98,17 @@ void satisfy_avl_right_tree(bst_t *tree, int rootValue, int *avlValid)
 	satisfy_avl_right_tree(tree->left, rootValue, avlValid);
 	satisfy_avl_right_tree(tree->right, rootValue, avlValid);
 }
+
+/**
+ * avl_duplicate_check - checks if there is a duplicate value in the tree
+ * before qualifying it as an avl tree.
+ * @tree: pointer to the root node of a tree to be qualified.
+ * @value: value to be checked against in finding duplicate/duplicates.
+ * @dupCheck: if 0, no duplicates found and thus tree is an avl tree.
+ * Otherwise, not an avl tree.
+ * Return: Nothing.
+ */
+
 void avl_duplicate_check(const bst_t *tree, int value, int *dupCheck)
 {
 	if (!tree)
@@ -73,11 +118,20 @@ void avl_duplicate_check(const bst_t *tree, int value, int *dupCheck)
 	{
 		*dupCheck = 1;
 		return;
-
 	}
 	avl_duplicate_check((const bst_t *) tree->left, value, dupCheck);
 	avl_duplicate_check((const bst_t *) tree->right, value, dupCheck);
 }
+
+/**
+ * avl_duplicate_check_caller - performs the duplicate check by recursively
+ * calling the appropriate functions.
+ * @tree: pointer to the root node of a tree.
+ * @dupCheck: if any duplicate is found it's value becomes 1 proving tree is
+ * not an avl tree. Otherwise, 0 meaning it can qualify as an avl tree.
+ * Return: dupCheck.
+ */
+
 int avl_duplicate_check_caller(const bst_t *tree, int *dupCheck)
 {
 	int value;
@@ -95,6 +149,14 @@ int avl_duplicate_check_caller(const bst_t *tree, int *dupCheck)
 	return (*dupCheck);
 
 }
+
+/**
+ * binary_tree_is_avl - proves a given tree is an avl tree if all tests are
+ * successfully passed.
+ * @tree: pointer to the root node of a given tree.
+ * Return: 1 if tree is an avl tree. Otherwise, 0.
+ */
+
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	int balanceCheck = 0, avlValid = 0, dupCheck = 0;
